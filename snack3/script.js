ef7706992496094b4458a31867af84c10162113d
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-10 12:06:44 +0100

    feat: snack 3

---
+++ snack3/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+const sommaNumeri = 0;
+
+for (let i = 0; i < 10; i++) {}
